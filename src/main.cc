883ecb457db56db11e47337036498e4c3a42216b
James Tateossian <jmtateossian50b@gmail.com>
2024-01-13 01:28:25 +0000

    removed unnecessary includes in main

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,3 @@
-#include <__expected/unexpect.h>
-#include <__expected/unexpected.h>
-#include <algorithm>
-#include <cctype>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <expected>
-
-#include "tokens.h"
 #include "repl.h"
 
 auto main(int argc, char**argv) -> int {
