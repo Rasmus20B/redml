82334766654d92e0f14d25b4a418e465b696a09c
James Tateossian <jmtateossian50b@gmail.com>
2024-01-13 01:17:15 +0000

    added scope to frontend, and num literal handling.

--- src/tokens.h
+++ src/tokens.h
@@ -1,22 +1,57 @@
 #pragma once
 
 #include <string>
+#include <string_view>
+#include <array>
 
 namespace redml {
 
-enum class TokenType {
-  TYPE_INT,
-  TYPE_VOID,
-  IDENT,
-  ADD,
-  SUB,
-  MUL,
-  DIV,
-  RETURN,
-};
 
-struct Token {
-  TokenType type;
-  std::string symbol;
-};
+  enum TokenType  {
+    TYPE_INT,
+    TYPE_VOID,
+    IDENT,
+    INT_LIT,
+    FLOAT_LIT,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACK,
+    RIGHT_BRACK,
+    LEFT_SQUARE,
+    RIGHT_SQUARE,
+    SEMI_COL,
+    COL,
+    RETURN,
+    SIZE
+  };
+
+  static constexpr std::array<std::string_view, TokenType::SIZE> TOKENSYMBOLS {{
+    "int",
+    "void",
+    "",
+    "",
+    "",
+    "+",
+    "-",
+    "*",
+    "/",
+    "(",
+    ")",
+    "{",
+    "}",
+    "[",
+    "]",
+    ";",
+    ":",
+    "return"
+  }};
+
+  struct Token {
+    TokenType type;
+    std::string symbol;
+  };
 }
